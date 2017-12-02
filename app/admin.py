d379480058241521ad052615b35cfce49bb6591c
Kennard <KennardZhang@gmail.com>
2017-12-03 00:24:17 +0800

    add

--- app/admin.py
+++ app/admin.py
@@ -3,7 +3,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Printer
+from .models import Profile, Printer, PrinterOptions, PrintJobs, RedeemCode
 
 
+admin.site.register(Profile)
 admin.site.register(Printer)
+admin.site.register(PrinterOptions)
+admin.site.register(PrintJobs)
+admin.site.register(RedeemCode)
--- app/models.py
+++ app/models.py
@@ -33,9 +33,10 @@ class Printer(models.Model):
     printer_name = models.CharField(max_length=128, help_text="打印机名称")
     command = models.CharField(max_length=128, help_text="打印命令", default="lp")
     device_name = models.CharField(max_length=128, help_text="设备名称")
-    host_name = models.CharField(max_length=128, help_text="主机名", blank=True, null=True)
-    port = models.IntegerField(help_text="端口", blank=True, null=True)
-    username = models.CharField(max_length=128, help_text="用户名", blank=True, null=True)
+    host_name = models.CharField(max_length=128, help_text="主机名（可选）", blank=True, null=True)
+    port = models.IntegerField(help_text="端口（可选）", blank=True, null=True)
+    username = models.CharField(
+        max_length=128, help_text="用户名（可选）", blank=True, null=True)
     media = models.CharField(max_length=128, help_text="介质命令")
     sides = models.CharField(max_length=128, help_text="双面打印命令")
     number_up = models.CharField(max_length=128, help_text="每页张数命令")
@@ -51,7 +52,93 @@ class Printer(models.Model):
 
     # Methods
     def get_absolute_url(self):
-         return reverse('model-detail-view', args=[str(self.id)])
+         return reverse('printer-detail', args=[str(self.id)])
 
     def __str__(self):
         return self.printer_name
+
+
+class PrinterOptions(models.Model):
+    """
+    打印机选项模型
+    """
+
+    # Fields
+    pid = models.IntegerField(help_text="打印机")
+    function = models.CharField(max_length=128, help_text="功能")
+    value = models.CharField(max_length=128, help_text="选项")
+    description = models.CharField(max_length=128, help_text="描述")
+    is_active = models.BooleanField(help_text="是否启用?")
+    # Metadata
+    class Meta:
+        ordering = ["-id"]
+
+    # Methods
+    def get_absolute_url(self):
+         return reverse('printer-option-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.id
+
+
+class PrintJobs(models.Model):
+    """
+    打印任务模型
+    """
+
+    # Fields
+    pid = models.IntegerField(help_text="所选打印机")
+    uid = models.IntegerField(help_text="用户")
+    upload = models.FileField(upload_to='uploads/%Y/%m/%d/', default="文件")
+    file_pages = models.IntegerField(help_text="文件页数")
+    verify = models.CharField(max_length=128, help_text="校验码")
+    sided = models.IntegerField(help_text="1 为单面打印双面打印，2 为长边装订双面打印，3 为短边装订双面打印")
+    number_up = models.IntegerField(help_text="每张页数", default=1)
+    number_up_layout = models.CharField(max_length=128, help_text="介质")
+    page_ranges = models.CharField(max_length=128, help_text="页面范围")
+    copies = models.IntegerField(help_text="份数", default=1)
+    print_pages = models.IntegerField(help_text="实际打印张数")
+    cost = models.DecimalField(max_digits=10, decimal_places=2, help_text="花费")
+    created_time = models.TimeField(help_text="任务创建时间")
+    is_printed = models.BooleanField(help_text="是否打印?")
+    printed_time = models.TimeField(help_text="任务打印时间（可选）", blank=True, null=True)
+
+
+    # Metadata
+    class Meta:
+        ordering = ["-id"]
+
+    # Methods
+    def get_absolute_url(self):
+         return reverse('job-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.id
+
+
+class RedeemCode(models.Model):
+    """
+    兑换码模型
+    """
+
+    # Fields
+    code = models.CharField(max_length=128, help_text="兑换码")
+    amount = models.DecimalField(max_digits=10, decimal_places=2, help_text="金额")
+    created_by = models.IntegerField(help_text="创建人")
+    created_time = models.TimeField(help_text="创建时间")
+    is_used = models.BooleanField(help_text="是否使用?")
+    used_by = models.IntegerField(help_text="使用者（可选）", blank=True, null=True)
+    used_time = models.TimeField(help_text="使用时间（可选）", blank=True, null=True)
+
+    # Metadata
+    class Meta:
+        ordering = ["-id"]
+
+    # Methods
+    def get_absolute_url(self):
+         return reverse('redeem-code-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.id
+
+
