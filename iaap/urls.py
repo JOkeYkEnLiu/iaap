8ded6119e6fef6cd89b4a705c4c616689404a694
Kennard <KennardZhang@gmail.com>
2017-12-03 08:59:11 +0800

    add

--- iaap/urls.py
+++ iaap/urls.py
@@ -18,4 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('library.urls')),
+    url(r'^library/', include('library.urls')),
 ]
