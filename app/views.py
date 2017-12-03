f0947a53e4ec1a205e874f3b0b64cc1cbfd19fe0
Kennard <KennardZhang@gmail.com>
2017-12-03 22:19:05 +0800

    add

--- app/views.py
+++ app/views.py
@@ -29,7 +29,7 @@ def user_login(request):
 
         if user:
             if remember_me:
-                request.session.set_expiry(60 * 3600 * 24 * 14)
+                request.session.set_expiry(30)
             if user.is_active:
                 auth.login(request, user)
                 return HttpResponseRedirect('/user/')
