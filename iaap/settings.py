76698724c7fe7483276a10f4f79922e604196959
Kennard <KennardZhang@gmail.com>
2017-12-03 11:47:25 +0800

    add

--- iaap/settings.py
+++ iaap/settings.py
@@ -133,3 +133,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
