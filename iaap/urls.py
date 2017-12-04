36ef08becfd6222a41e7fbd00ec5a01e9fe5fbf7
Kennard <KennardZhang@gmail.com>
2017-12-04 14:16:20 +0800

    add

--- iaap/urls.py
+++ iaap/urls.py
@@ -18,6 +18,13 @@ from django.contrib import admin
 
 import app.views as views
 from django.conf.urls import handler404, handler500
+from django.conf import settings
+if settings.DEBUG is False:
+    urlpatterns += patterns('',
+                            url(r'^assets/(?P<path>.*)$', 'django.views.static.serve', {
+                                'document_root': settings.STATIC_ROOT,
+                            }),
+                            )
 
 handler404 = views.page_error
 handler500 = views.page_error
