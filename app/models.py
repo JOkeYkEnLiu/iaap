3ddf316acbfcd842ba1deefe3f728bfb979fee42
Kennard <KennardZhang@gmail.com>
2017-12-06 13:20:49 +0800

    add

--- app/models.py
+++ app/models.py
@@ -159,7 +159,7 @@ class PrintJobs(models.Model):
     pid = models.IntegerField(
         choices=PID_CHOICES, help_text="所选打印机",)
     uid = models.IntegerField(help_text="用户",)
-    upload = models.FileField(upload_to='uploads/%Y/%m/%d', default="文件",)
+    upload = models.FileField(upload_to='uploads', default="文件",)
     file_pages = models.IntegerField(help_text="文件页数",)
     verify = models.CharField(max_length=128, help_text="校验码",)
     sided = models.IntegerField(
