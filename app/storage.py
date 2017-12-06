e59e5062a1a721df94bb959482dc9d025af9ef53
Kennard <KennardZhang@gmail.com>
2017-12-06 11:30:43 +0800

    add

--- app/storage.py
+++ app/storage.py
@@ -8,13 +8,13 @@ class PdfStorage(FileSystemStorage):
 
     def __init__(self, location=settings.MEDIA_ROOT,   base_url=settings.MEDIA_URL):
         # 初始化
-    super(PdfStorage, self).__init__(location, base_url)
+        super(PdfStorage, self).__init__(location, base_url)
     # 重写 _save方法
 
     def _save(self, name, content):
-    import os
-    import time
-    import random
+        import os
+        import time
+        import random
     # 文件扩展名
         ext = os.path.splitext(name)[1]
         # 文件目录
