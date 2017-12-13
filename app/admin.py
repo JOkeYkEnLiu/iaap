43d73adeba754f97218b8423203eb9bfe204d14d
Kennard <KennardZhang@gmail.com>
2017-12-13 15:05:14 +0800

    add

--- app/admin.py
+++ app/admin.py
@@ -3,7 +3,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile, BalanceLog, Printer, PrinterOptions, RedeemCode, PrintJobs, paysAPI
+from .models import Profile, BalanceLog, Printer, PrinterOptions, RedeemCode, PrintJobs, paysAPI, Order
 
 
 admin.site.register(Profile)
@@ -12,4 +12,5 @@ admin.site.register(Printer)
 admin.site.register(PrinterOptions)
 admin.site.register(PrintJobs)
 admin.site.register(RedeemCode)
+admin.site.register(Order)
 admin.site.register(paysAPI)
