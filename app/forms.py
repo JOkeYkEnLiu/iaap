661f10e910fee0e49583004840945d5b1954896c
Kennard <KennardZhang@gmail.com>
2017-12-13 16:02:29 +0800

    add

--- app/forms.py
+++ app/forms.py
@@ -68,7 +68,7 @@ class QuickNewOrderForm(forms.Form):
     pid = forms.IntegerField(widget=forms.Select(attrs={'class': 'form-control m-input',"value":"1"}))
     sided = forms.IntegerField(widget=forms.Select(attrs={'class': 'form-control m-input',"value":"1"}))
     number_up = forms.IntegerField(widget=forms.NumberInput(attrs={"type": "hidden", "value": 1}))
-    number_up_layout = forms.CharFieldm(max_length=100,widget=forms.TextInput(attrs = {"type": "hidden", "value": "tblr"}))
+    number_up_layout = forms.CharField(max_length=100,widget=forms.TextInput(attrs = {"type": "hidden", "value": "tblr"}))
     media = forms.CharField(max_length=128,widget=forms.TextInput(attrs={"type": "hidden", "value": "A4"}))
     page_ranges = forms.CharField(max_length=128,widget=forms.TextInput(attrs={"type": "hidden", "value": ""}))
     copies = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control m-input', "type": "number", "value": 1}))
